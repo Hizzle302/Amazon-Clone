628452beb17340b964fbad7e13893cd35291f39b
Hizzle 302 <azcrawley@yahoo.com>
2021-02-28 17:32:20 -0500

    got image to show

--- app.js
+++ app.js
@@ -1,22 +1,51 @@
-// I want to make a card with a product number & a price & an image
+const productData = [{ 
+    img: "images2/lightred.jpg",
+    h5: "Product 1",
+    p: "25.99",
+    button: "Add to Cart"
+},
 
-const table = document.getElementsByClassName('table')[0];
-console.log(table);
+{ 
+    img: "images2/images-3.jpg",
+    h5: "Product 1",
+    p: "25.99",
+    button: "Add to Cart"
+}]
 
-function createCard(number) {
-    const card = document.createElement('div');
-    card.className = "card";
+const container = document.getElementById('grid');
 
-    const productNumber = document.createElement('div');
-    productNumber.innerText = number;
+productData.forEach((result) => {
 
-    const productPrice = document.createElement('div');
-    productPrice.innerText = number;
 
-    card.append(productNumber);
-    card.append(productPrice);
 
-    return card;
-}
+const card = document.createElement('div');
 
-table.appendChild(createCard(12.99));
+const content = ` <div class="container">
+<div class="row">
+<div class="col col-md-3">
+<div class="card h-100"> 
+<div id="cards">
+<img src="${result.img}" class="card-img-top" alt="blue-shirt">
+<h5 class="card-title">${result.h5}</h5>
+<p class="text-center">${result.p}</p>
+</div>
+<div class="card-footer">
+<div class="d-grid gap-2 col-6 mx-auto">
+<button id="button" class="btn btn-secondary btn-sm">${result.button}</button>
+</div>
+<small class="text-muted"></small>
+</div>
+
+
+</div>
+
+</div>
+</div>
+</div>`
+    
+
+container.innerHTML += content;
+})
+
+
+
